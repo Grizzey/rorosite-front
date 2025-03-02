035df5ac0be7bfd2169812f7f76d1faf909f25a8
Terence Montecillo <4hoursleft@gmail.com>
2025-03-02 22:01:31 +0800

    Updated design (parang?)

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//BLANK
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("navbar");
+    const navLinks = document.querySelectorAll(".nav-link");
+    const menuButton = document.getElementById("menu-button");
+    const menu = document.getElementById("mobile-menu");
+
+    function updateNavbar() {
+        if (window.scrollY > 20) {
+            navbar.classList.remove("bg-transparent");
+            navbar.classList.add("bg-blue-400", "shadow-md");
+        } else {
+            navbar.classList.add("bg-transparent");
+            navbar.classList.remove("bg-blue-400", "shadow-md");
+        }
+    }
+
+    updateNavbar();
+    window.addEventListener("scroll", updateNavbar);
+
+    //Mobile
+    menuButton.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+});
