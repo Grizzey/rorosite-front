590b7b042e56dc2783744fed54dbe11f0eeaebbd
Terence Montecillo <4hoursleft@gmail.com>
2025-05-19 01:00:28 +0800

    Improves email regex

--- scripts/submit_register.js
+++ scripts/submit_register.js
@@ -26,7 +26,7 @@ async function submitRegister() {
 
 
     // Email validation regex
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
 
     let isValid = true;
 
